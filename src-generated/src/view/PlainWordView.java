13d257a36c011e5bdced419d4d2cf898699fd2fb
nectarius <nectarius@outlook.com>
2013-11-17 12:39:29 +0400

    add views

---
+++ src-generated/src/view/PlainWordView.java
@@ -0,0 +1,51 @@
+// -- DO NOT EDIT  -  THIS CODE WILL BE REGENERATED! --
+package view;
+
+public class PlainWordView {
+
+	private java.lang.Long id;
+	private java.lang.String word;
+	private java.lang.String translate;
+	private java.lang.Integer priority;
+	private java.lang.String whence;
+
+    public PlainWordView() {
+
+	}
+	
+		public java.lang.Long getId() {
+			return id;
+		}
+	
+		public void setId(java.lang.Long id) {
+			this.id = id;
+		}
+		public java.lang.String getWord() {
+			return word;
+		}
+	
+		public void setWord(java.lang.String word) {
+			this.word = word;
+		}
+		public java.lang.String getTranslate() {
+			return translate;
+		}
+	
+		public void setTranslate(java.lang.String translate) {
+			this.translate = translate;
+		}
+		public java.lang.Integer getPriority() {
+			return priority;
+		}
+	
+		public void setPriority(java.lang.Integer priority) {
+			this.priority = priority;
+		}
+		public java.lang.String getWhence() {
+			return whence;
+		}
+	
+		public void setWhence(java.lang.String whence) {
+			this.whence = whence;
+		}
+}
