40b2dc9c00561c95a26920791b53cc3bd10263d1
nectarius <nectarius@outlook.com>
2014-05-18 00:46:12 +0400

    add event controller

---
+++ src/main/java/controller/EventController.java
@@ -0,0 +1,97 @@
+package controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import service.EventService;
+import view.PageEventView;
+import view.PlainEventView;
+
+import javax.validation.Valid;
+
+/**
+ * Created by nectarius on 11/16/13.
+ */
+@Controller
+public class EventController {
+
+    @Autowired
+    private EventService EventService;
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(EventController.class.getName());
+
+    /**
+     * @param id identity
+     * @return data view
+     */
+    @RequestMapping(value = {"/event/{id}.data"}, method = RequestMethod.GET)
+    @ResponseBody
+    public PlainEventView findEvent(@PathVariable("id") Long id) {
+
+        return EventService.findOne(id);
+
+    }
+
+    /**
+     * delete data for Event
+     *
+     * @param id identity
+     * @return
+     */
+    @RequestMapping(value = {"/event/remove/{id}.data"}, method = RequestMethod.DELETE)
+    public ResponseEntity<String> removeEvent(@PathVariable("id") Long id) {
+
+        if (id != null) {
+            EventService.deleteEvent(id);
+            return new ResponseEntity<String>(HttpStatus.OK);
+        } else {
+            LOGGER.warn("Request event id is null");
+            return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+    /**
+     * Сохранение данных
+     *
+     * @param EventView data view for a Event
+     * @param result       info
+     * @return
+     */
+    @RequestMapping(value = {"/event/save.data"}, method = RequestMethod.POST)
+    public ResponseEntity<String> saveEvent(@Valid @RequestBody PlainEventView EventView, BindingResult result) {
+
+        if (result.hasErrors()) {
+            LOGGER.info("incorrect data: {}", result.getAllErrors());
+            return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+        } else {
+            EventService.saveOrUpdateEvent(EventView);
+            return new ResponseEntity<String>(HttpStatus.OK);
+        }
+
+    }
+
+    /**
+     * @param pageNumber page number
+     * @param pageSize   quantity field in one page
+     * @param direction  desending or assending
+     * @param column     column for sorting
+     * @return Event page
+     */
+    @RequestMapping(value = "eventlist/{pageNumber}/{pageSize}/{direction}/sortBy{column}.data", method = RequestMethod.GET)
+    @ResponseBody
+    public PageEventView findEventList(@PathVariable("pageNumber") Integer pageNumber, @PathVariable("pageSize") Integer pageSize, @PathVariable("direction") String direction, @PathVariable("column") String column) {
+
+        return EventService.findAllEventList(pageNumber, pageSize, direction, column);
+
+    }
+
+
+
+
+}
--- src/main/webapp/js/harvester.js
+++ src/main/webapp/js/harvester.js
@@ -2,9 +2,15 @@
 angular.module('guestpage',
     ['ngResource', 'newguest', 'editguest' , 'guestlist'])
     .config(['$routeProvider', function ($routeProvider) {
+
         $routeProvider.when('/guestlist', {templateUrl: 'components/guestlist/guestlist.html', controller: 'guestlist'});
         $routeProvider.when('/newguest', {templateUrl: 'components/guest/newguest.html', controller: 'Newguest'});
         $routeProvider.when('/editguest/:id', {templateUrl: 'components/guest/editguest.html', controller: 'Editguest'});
+
+        $routeProvider.when('/eventlist', {templateUrl: 'components/eventlist/eventlist.html', controller: 'eventlist'});
+        $routeProvider.when('/newevent', {templateUrl: 'components/event/newevent.html', controller: 'Newevent'});
+        $routeProvider.when('/editevent/:id', {templateUrl: 'components/event/editevent.html', controller: 'Editevent'});
+
         $routeProvider.otherwise({redirectTo: '/guestlist'});
     }]);
 
