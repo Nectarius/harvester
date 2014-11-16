d073b0650702c2c0c54f751b82ff0b7ddd9fa9e8
nectarius <nectarius@outlook.com>
2014-11-17 00:45:22 +0400

    reports about guests by event

--- src/main/java/service/report/ExporterService.java
+++ src/main/java/service/report/ExporterService.java
@@ -3,10 +3,9 @@ package service.report;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JRExporterParameter;
 import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.export.HtmlExporter;
-import net.sf.jasperreports.engine.export.JRPdfExporter;
-import net.sf.jasperreports.engine.export.JRXlsAbstractExporterParameter;
-import net.sf.jasperreports.engine.export.JRXlsExporter;
+import net.sf.jasperreports.engine.export.*;
+import net.sf.jasperreports.engine.export.ooxml.JRDocxExporter;
+import net.sf.jasperreports.engine.export.ooxml.JRPptxExporter;
 import net.sf.jasperreports.export.*;
 import org.springframework.stereotype.Service;
 
@@ -24,9 +23,23 @@ public class ExporterService {
     public static final String MEDIA_TYPE_EXCEL = "application/vnd.ms-excel";
     public static final String MEDIA_TYPE_PDF = "application/pdf";
     public static final String MEDIA_TYPE_HTML = "application/html";
+    public static final String MEDIA_TYPE_RTF = "application/rtf";
+    public static final String MEDIA_TYPE_DOCX = "application/docx";
+    public static final String MEDIA_TYPE_CSV= "application/csv";
+    public static final String MEDIA_TYPE_TEXT= "application/text";
+    public static final String MEDIA_TYPE_JSON= "application/json";
+    public static final String MEDIA_TYPE_XML= "application/xml";
+    public static final String MEDIA_TYPE_PPTX= "application/pptx";
     public static final String EXTENSION_TYPE_EXCEL = "xls";
     public static final String EXTENSION_TYPE_PDF = "pdf";
     public static final String EXTENSION_TYPE_HTML = "html";
+    public static final String EXTENSION_TYPE_RTF = "rtf";
+    public static final String EXTENSION_TYPE_DOCX = "docx";
+    public static final String EXTENSION_TYPE_CSV = "csv";
+    public static final String EXTENSION_TYPE_TEXT = "text";
+    public static final String EXTENSION_TYPE_JSON = "json";
+    public static final String EXTENSION_TYPE_XML = "xml";
+    public static final String EXTENSION_TYPE_PPTX = "pptx";
 
     public HttpServletResponse export(String type,
                                       JasperPrint jp,
@@ -44,6 +57,70 @@ public class ExporterService {
 
             // Set content type
             response.setContentType(MEDIA_TYPE_EXCEL);
+            response.setContentLength(baos.size());
+
+            return response;
+        }
+
+        if (type.equalsIgnoreCase(EXTENSION_TYPE_TEXT)) {
+            // Export to output stream
+            exportText(jp, baos);
+
+            // Set our response properties
+            // Here you can declare a custom filename
+            String fileName = "Report.txt";
+            response.setHeader("Content-Disposition", "inline; filename=" + fileName);
+
+            // Set content type
+            response.setContentType(MEDIA_TYPE_TEXT);
+            response.setContentLength(baos.size());
+
+            return response;
+        }
+
+        if (type.equalsIgnoreCase(EXTENSION_TYPE_RTF)) {
+            // Export to output stream
+            exportRTF(jp, baos);
+
+            // Set our response properties
+            // Here you can declare a custom filename
+            String fileName = "Report.rtf";
+            response.setHeader("Content-Disposition", "inline; filename=" + fileName);
+
+            // Set content type
+            response.setContentType(MEDIA_TYPE_RTF) ;
+            response.setContentLength(baos.size());
+
+            return response;
+        }
+
+        if (type.equalsIgnoreCase(EXTENSION_TYPE_DOCX)) {
+            // Export to output stream
+            exportDOCX(jp, baos);
+
+            // Set our response properties
+            // Here you can declare a custom filename
+            String fileName = "Report.docx";
+            response.setHeader("Content-Disposition", "inline; filename=" + fileName);
+
+            // Set content type
+            response.setContentType(MEDIA_TYPE_DOCX) ;
+            response.setContentLength(baos.size());
+
+            return response;
+        }
+
+        if (type.equalsIgnoreCase(EXTENSION_TYPE_CSV)) {
+            // Export to output stream
+            exportCSV(jp, baos);
+
+            // Set our response properties
+            // Here you can declare a custom filename
+            String fileName = "Report.csv";
+            response.setHeader("Content-Disposition", "inline; filename=" + fileName);
+
+            // Set content type
+            response.setContentType(MEDIA_TYPE_CSV) ;
             response.setContentLength(baos.size());
 
             return response;
@@ -77,6 +154,57 @@ public class ExporterService {
 
             // Set content type
             response.setContentType(MEDIA_TYPE_HTML);
+            response.setContentLength(baos.size());
+
+            return response;
+
+        }
+
+        if (type.equalsIgnoreCase(EXTENSION_TYPE_PPTX)) {
+            // Export to output stream
+            exportPPTX(jp, baos);
+
+            // Set our response properties
+            // Here you can declare a custom filename
+            String fileName = "Report.pptx";
+            response.setHeader("Content-Disposition", "inline; filename="+ fileName);
+
+            // Set content type
+            response.setContentType(MEDIA_TYPE_PPTX);
+            response.setContentLength(baos.size());
+
+            return response;
+
+        }
+
+        if (type.equalsIgnoreCase(EXTENSION_TYPE_XML)) {
+            // Export to output stream
+            exportXML(jp, baos);
+
+            // Set our response properties
+            // Here you can declare a custom filename
+            String fileName = "Report.xml";
+            response.setHeader("Content-Disposition", "inline; filename="+ fileName);
+
+            // Set content type
+            response.setContentType(MEDIA_TYPE_XML);
+            response.setContentLength(baos.size());
+
+            return response;
+
+        }
+
+        if (type.equalsIgnoreCase(EXTENSION_TYPE_JSON)) {
+            // Export to output stream
+            exportJSON(jp, baos);
+
+            // Set our response properties
+            // Here you can declare a custom filename
+            String fileName = "Report.txt";
+            response.setHeader("Content-Disposition", "inline; filename="+ fileName);
+
+            // Set content type
+            response.setContentType(MEDIA_TYPE_JSON);
             response.setContentLength(baos.size());
 
             return response;
@@ -138,6 +266,228 @@ public class ExporterService {
         //SimpleHtmlExporterConfiguration configuration = new SimpleHtmlExporterConfiguration();
         //configuration.setFlushOutput();
         //exporter.setConfiguration(configuration);
+
+        //exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, baos);
+
+        try {
+            exporter.exportReport();
+
+        } catch (JRException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void exportRTF(JasperPrint jp, ByteArrayOutputStream baos) {
+
+        // Create a JRXlsExporter instance
+        JRRtfExporter exporter= new JRRtfExporter();
+
+        List<JasperPrint> jasperPrintList = new ArrayList<JasperPrint>();
+        jasperPrintList.add(jp);
+
+        // Here we assign the parameters jp and baos to the exporter
+        exporter.setExporterInput(SimpleExporterInput.getInstance(jasperPrintList));
+
+        WriterExporterOutput rtfExporterOutput = new  SimpleWriterExporterOutput(baos);
+
+        exporter.setExporterOutput(rtfExporterOutput);
+
+        //SimpleHtmlExporterConfiguration configuration = new SimpleHtmlExporterConfiguration();
+        //configuration.setFlushOutput();
+        //exporter.setConfiguration(configuration);
+
+        //exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, baos);
+
+        try {
+            exporter.exportReport();
+
+        } catch (JRException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void exportCSV(JasperPrint jp, ByteArrayOutputStream baos) {
+
+        // Create a JRXlsExporter instance
+        JRAbstractCsvExporter exporter= new JRCsvExporter();
+
+        List<JasperPrint> jasperPrintList = new ArrayList<JasperPrint>();
+        jasperPrintList.add(jp);
+
+        // Here we assign the parameters jp and baos to the exporter
+        exporter.setExporterInput(SimpleExporterInput.getInstance(jasperPrintList));
+
+        WriterExporterOutput csvExporterOutput = new  SimpleWriterExporterOutput(baos);
+
+        exporter.setExporterOutput(csvExporterOutput);
+
+        //SimpleHtmlExporterConfiguration configuration = new SimpleHtmlExporterConfiguration();
+        //configuration.setFlushOutput();
+        //exporter.setConfiguration(configuration);
+
+        //exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, baos);
+
+        try {
+            exporter.exportReport();
+
+        } catch (JRException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void exportDOCX(JasperPrint jp, ByteArrayOutputStream baos) {
+
+        // Create a JRXlsExporter instance
+        JRDocxExporter exporter= new JRDocxExporter();
+
+        List<JasperPrint> jasperPrintList = new ArrayList<JasperPrint>();
+        jasperPrintList.add(jp);
+
+        // Here we assign the parameters jp and baos to the exporter
+        exporter.setExporterInput(SimpleExporterInput.getInstance(jasperPrintList));
+
+        SimpleOutputStreamExporterOutput rtfExporterOutput = new  SimpleOutputStreamExporterOutput(baos);
+
+        exporter.setExporterOutput(rtfExporterOutput);
+
+        //SimpleHtmlExporterConfiguration configuration = new SimpleHtmlExporterConfiguration();
+        //configuration.setFlushOutput();
+        //exporter.setConfiguration(configuration);
+
+        //exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, baos);
+
+        try {
+            exporter.exportReport();
+
+        } catch (JRException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void exportPPTX(JasperPrint jp, ByteArrayOutputStream baos) {
+
+        // Create a JRXlsExporter instance
+        JRPptxExporter exporter= new JRPptxExporter();
+
+        List<JasperPrint> jasperPrintList = new ArrayList<JasperPrint>();
+        jasperPrintList.add(jp);
+
+        // Here we assign the parameters jp and baos to the exporter
+        exporter.setExporterInput(SimpleExporterInput.getInstance(jasperPrintList));
+
+        SimpleOutputStreamExporterOutput pptxExporterOutput = new  SimpleOutputStreamExporterOutput(baos);
+
+        exporter.setExporterOutput(pptxExporterOutput);
+
+        //SimpleHtmlExporterConfiguration configuration = new SimpleHtmlExporterConfiguration();
+        //configuration.setFlushOutput();
+        //exporter.setConfiguration(configuration);
+
+        //exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, baos);
+
+        try {
+            exporter.exportReport();
+
+        } catch (JRException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void exportXML(JasperPrint jp, ByteArrayOutputStream baos) {
+
+        // Create a JRXlsExporter instance
+        JRXmlExporter exporter= new JRXmlExporter();
+
+        List<JasperPrint> jasperPrintList = new ArrayList<JasperPrint>();
+        jasperPrintList.add(jp);
+
+        // Here we assign the parameters jp and baos to the exporter
+        exporter.setExporterInput(SimpleExporterInput.getInstance(jasperPrintList));
+
+        WriterExporterOutput xmlExporterOutput = new  SimpleWriterExporterOutput(baos);
+
+        exporter.setExporterOutput(xmlExporterOutput);
+
+        //SimpleHtmlExporterConfiguration configuration = new SimpleHtmlExporterConfiguration();
+        //configuration.setFlushOutput();
+        //exporter.setConfiguration(configuration);
+
+        //exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, baos);
+
+        try {
+            exporter.exportReport();
+
+        } catch (JRException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void exportJSON(JasperPrint jp, ByteArrayOutputStream baos) {
+
+        // Create a JRXlsExporter instance
+        JsonExporter exporter= new JsonExporter();
+
+        List<JasperPrint> jasperPrintList = new ArrayList<JasperPrint>();
+        jasperPrintList.add(jp);
+
+        // Here we assign the parameters jp and baos to the exporter
+        exporter.setExporterInput(SimpleExporterInput.getInstance(jasperPrintList));
+
+        WriterExporterOutput jsonExporterOutput = new  SimpleWriterExporterOutput(baos);
+
+        exporter.setExporterOutput(jsonExporterOutput);
+
+        //SimpleHtmlExporterConfiguration configuration = new SimpleHtmlExporterConfiguration();
+        //configuration.setFlushOutput();
+        //exporter.setConfiguration(configuration);
+
+        //exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, baos);
+
+        try {
+            exporter.exportReport();
+
+        } catch (JRException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+    public void exportText(JasperPrint jp, ByteArrayOutputStream baos) {
+
+        // Create a JRXlsExporter instance
+        JRTextExporter exporter= new JRTextExporter();
+
+
+        List<JasperPrint> jasperPrintList = new ArrayList<JasperPrint>();
+        jasperPrintList.add(jp);
+
+        // Here we assign the parameters jp and baos to the exporter
+        SimpleExporterInput exporterInput = SimpleExporterInput.getInstance(jasperPrintList);
+
+
+        exporter.setExporterInput(exporterInput);
+
+        WriterExporterOutput textExporterOutput = new  SimpleWriterExporterOutput(baos);
+
+
+
+        exporter.setExporterOutput(textExporterOutput);
+
+        SimpleTextReportConfiguration reportConfiguration = new SimpleTextReportConfiguration();
+
+        reportConfiguration.setPageHeightInChars(50);
+
+        reportConfiguration.setPageWidthInChars(80);
+
+        SimpleTextExporterConfiguration configuration = new SimpleTextExporterConfiguration();
+
+        exporter.setConfiguration(reportConfiguration);
+        //configuration.setLineSeparator("");
+        //configuration.setFlushOutput();
+        configuration.setLineSeparator("-");
+        configuration.setPageSeparator("/");
+        exporter.setConfiguration(configuration);
 
         //exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, baos);
 
