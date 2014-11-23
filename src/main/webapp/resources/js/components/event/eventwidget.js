fe162de7260ee159834bf4bd309d80bc74a02ad3
nectarius <nectarius@outlook.com>
2014-11-23 04:28:56 +0400

    validations {notes & events}

--- src/main/webapp/resources/js/components/event/eventwidget.js
+++ src/main/webapp/resources/js/components/event/eventwidget.js
@@ -4,6 +4,15 @@ angular.module('event', ['ngResource'])
             restrict: 'E',
             priority: 5,
             transclude: true,
+            link: function(scope, element, attrs, ngModel){
+                function isInvalid(){
+                    // it's something to think about ...
+                    return scope.form.$invalid;
+                }
+                scope.$watch(isInvalid, function(newValue, oldValue){
+                    scope.model.invalid = newValue;
+                });
+            },
             scope: {
                 model: '=model'
 
